44f26fe7cd12909999dbb8aa8e31724efeda20d3
yingxi1 <yingxi.luo@outlook.com>
2022-04-23 21:54:26 +0800

    fix the dashboard data display and the  model

---
+++ models/patients.js
@@ -0,0 +1,11 @@
+module.exports = [
+    {
+        patientName: 'Pat',
+        age: '36',
+        gender: 'male',
+        Blood_glucose_level : '180',
+        Weight: '65',
+        Insulin_taken: '3',
+        Exercise: '5000'
+    }
+]
