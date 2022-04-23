91afa8da48b62df2248e2d0885945624be76d68c
yingxi1 <yingxi.luo@outlook.com>
2022-04-23 23:48:40 +0800

    update db

--- models/patients.js
+++ models/patients.js
@@ -3,9 +3,21 @@ module.exports = [
         patientName: 'Pat',
         age: '36',
         gender: 'male',
-        blood_glucose_level : '180',
-        weight: '65',
-        insulin_taken: '3',
-        exercise: '5000'
+        blood_glucose_level : [
+            '180',
+            '179'
+        ],
+        weight: [
+            '65.0',
+            '64.8'
+        ],
+        insulin_taken: [
+            '3',
+            '3'
+        ],
+        exercise: [
+            '5000',
+            '5000'
+        ]
     }
 ]
