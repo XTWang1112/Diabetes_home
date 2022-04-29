486b923a6ff11a7a7423814e6ac8a718d38050a2
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-04-29 21:23:10 +1000

    hide

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,17 +1,18 @@
 const req = require("express/lib/request");
 const mongoose = require("mongoose");
 const patientData = require("../models/patient");
-const pd = require("../models/patients");
 const patientModel = mongoose.model("patients");
 const bloodGlucoseModel = mongoose.model("bloodGlucoses");
 const weightModel = mongoose.model("weights");
-const insulineTakenModel = mongoose.model("insulinTakens")
-const exerciseModel = mongoose.model("exercises")
-
+const insulineTakenModel = mongoose.model("insulinTakens");
+const exerciseModel = mongoose.model("exercises");
 
 const renderClinicianDashboard = async (req, res) => {
-  try{
-      const patients = await patientModel.find({}, {
+  try {
+    const patients = await patientModel
+      .find(
+        {},
+        {
           patientName: true,
           patientID: true,
           age: true,
@@ -21,122 +22,128 @@ const renderClinicianDashboard = async (req, res) => {
 
           bloodGlucose_lowerBound: true,
           bloodGlucose_upperBound: true,
-      }).lean();
+        }
+      )
+      .lean();
 
-      const today = new Date(new Date().toDateString()).getTime()
-      const tomorrow = today + 24 * 3600 * 1000
+    const today = new Date(new Date().toDateString()).getTime();
+    const tomorrow = today + 24 * 3600 * 1000;
 
-      for(patient of patients){
-          console.log(patient.patientName);
+    for (patient of patients) {
+      console.log(patient.patientName);
 
-          let query = {
-              patient_id: patient._id,
-              time: {$gte: today},
-              time: {$lte: tomorrow},
-          }
-          console.log(query)
-          let bloodGlucose_result = await bloodGlucoseModel.findOne(query).sort({
-              _id: -1,
-          })
+      let query = {
+        patient_id: patient._id,
+        time: { $gte: today },
+        time: { $lte: tomorrow },
+      };
+      console.log(query);
+      let bloodGlucose_result = await bloodGlucoseModel.findOne(query).sort({
+        _id: -1,
+      });
 
-          let patient_result = await patientModel.findOne(query)
-          
-          if(bloodGlucose_result){
-              console.log(bloodGlucose_result)
-              patient.today_blood_glucose_level = bloodGlucose_result.value
-              patient.timestamp_blood_glucose_level = bloodGlucose_result.time
-              patient.blood_glucose_level_lower_bound = patient_result.bloodGlucose_lowerBound
-              patient.blood_glucose_level_upper_bound = patient_result.bloodGlucose_upperBound
-          }else{
-              patient.today_blood_glucose_level = 0
-              patient.today_blood_glucose_level = "No data today"
-          }
+      let patient_result = await patientModel.findOne(query);
 
-          let weight_result = await weightModel.findOne(query).sort({
-            _id: -1,
-          })
-          if(weight_result){
-              console.log(weight_result)
-              patient.today_weight = weight_result.value
-              patient.timestamp_weight = weight_result.time
-              patient.weight_lower_bound = 40
-              patient.weight_upper_bound = 85
-          }else{
-              patient.today_weight = 0
-              patient.today_weight = "No data today"
-          }
+      if (bloodGlucose_result) {
+        console.log(bloodGlucose_result);
+        patient.today_blood_glucose_level = bloodGlucose_result.value;
+        patient.timestamp_blood_glucose_level = bloodGlucose_result.time;
+        patient.blood_glucose_level_lower_bound =
+          patient_result.bloodGlucose_lowerBound;
+        patient.blood_glucose_level_upper_bound =
+          patient_result.bloodGlucose_upperBound;
+      } else {
+        patient.today_blood_glucose_level = 0;
+        patient.today_blood_glucose_level = "No data today";
       }
-      console.log(patients);
-      res.render("Clinician_dashboard", {patients: patients});
-  } catch(err){
-      console.log(err);
+
+      let weight_result = await weightModel.findOne(query).sort({
+        _id: -1,
+      });
+      if (weight_result) {
+        console.log(weight_result);
+        patient.today_weight = weight_result.value;
+        patient.timestamp_weight = weight_result.time;
+        patient.weight_lower_bound = 40;
+        patient.weight_upper_bound = 85;
+      } else {
+        patient.today_weight = 0;
+        patient.today_weight = "No data today";
+      }
+    }
+    console.log(patients);
+    res.render("Clinician_dashboard", { patients: patients });
+  } catch (err) {
+    console.log(err);
   }
 };
 
-const renderPatientDashboard = async(req, res) => {
-    let patient_id = '6267d6bb8b206aade8b24198'
-    let patient = await patientModel.findById(patient_id).lean()
-    console.log(patient.patientName);
+const renderPatientDashboard = async (req, res) => {
+  let patient_id = "6267d6bb8b206aade8b24198";
+  let patient = await patientModel.findById(patient_id).lean();
+  console.log(patient.patientName);
 
-    const today = new Date(new Date().toDateString()).getTime()
-    const tomorrow = today + 24 * 3600 * 1000
-    let query = {
-        patient_id: patient._id,
-        time: {$gte: today},
-        time: {$lte: tomorrow},
+  const today = new Date(new Date().toDateString()).getTime();
+  const tomorrow = today + 24 * 3600 * 1000;
+  let query = {
+    patient_id: patient._id,
+    time: { $gte: today },
+    time: { $lte: tomorrow },
+  };
+  console.log(query);
+  let bloodGlucose_result = await bloodGlucoseModel.findOne(query).sort({
+    _id: -1,
+  });
+
+  let patient_result = await patientModel.findOne(query);
+  comments = [];
+  if (bloodGlucose_result) {
+    console.log(bloodGlucose_result);
+    patient.today_blood_glucose_level = bloodGlucose_result.value;
+    patient.timestamp_blood_glucose_level = bloodGlucose_result.time;
+    patient.blood_glucose_level_lower_bound =
+      patient_result.bloodGlucose_lowerBound;
+    patient.blood_glucose_level_upper_bound =
+      patient_result.bloodGlucose_upperBound;
+    // comments.push(bloodGlucose_result.comment)
+  } else {
+    patient.today_blood_glucose_level = 0;
+    patient.today_blood_glucose_level = "no data today";
+  }
+
+  console.log(comments);
+  res.render("Patient_Dashboard", {
+    patient,
+    comments,
+    layout: "patient_template",
+  });
+};
+
+const renderPatientBloodRecord = async (req, res) => {
+  const data = patientData;
+  if (data) {
+    res.render("Blood_glucose", {
+      onePatient: data,
+      layout: "patient_record_template",
+    });
+    var glucose_comment = req.query.glucose_comment || "no comments";
+    var patinet_blood_glucose = req.query.patinet_blood_glucose;
+    if (glucose_comment && patinet_blood_glucose) {
+      let patient_id = "6267d6bb8b206aade8b24198";
+      let patientBloodRecord = {
+        patient_id,
+        value: patinet_blood_glucose,
+        comment: glucose_comment,
+        time: new Date().getTime(),
+      };
+      await bloodGlucoseModel.create({
+        ...patientBloodRecord,
+      });
     }
-    console.log(query)
-    let bloodGlucose_result = await bloodGlucoseModel.findOne(query).sort({
-        _id: -1,
-    })
-
-    let patient_result = await patientModel.findOne(query)
-    comments = []
-    if(bloodGlucose_result){
-        console.log(bloodGlucose_result)
-        patient.today_blood_glucose_level = bloodGlucose_result.value
-        patient.timestamp_blood_glucose_level = bloodGlucose_result.time
-        patient.blood_glucose_level_lower_bound = patient_result.bloodGlucose_lowerBound
-        patient.blood_glucose_level_upper_bound = patient_result.bloodGlucose_upperBound
-        // comments.push(bloodGlucose_result.comment)
-    }else{
-        patient.today_blood_glucose_level = 0
-        patient.today_blood_glucose_level = "no data today"
-    }
-
-    console.log(comments)
-    res.render("Patient_Dashboard", {
-        patient,
-        comments,
-        layout: "patient_template",
-    })
-}
-
-const renderPatientBloodRecord = async(req, res) => {
-    const data = patientData;
-    if(data){
-        res.render("Blood_glucose", {
-            onePatient: data,
-            layout: "patient_record_template",
-        });
-        var glucose_comment = req.query.glucose_comment;
-        var patinet_blood_glucose = req.query.patinet_blood_glucose;
-        if (glucose_comment && patinet_blood_glucose) {
-            let patient_id = '6267d6bb8b206aade8b24198'
-            let patientBloodRecord = {
-                patient_id,
-                value: patinet_blood_glucose,
-                comment: glucose_comment,
-                time: new Date().getTime(),
-            }
-            await bloodGlucoseModel.create({
-                ...patientBloodRecord
-            })
-        }
-    }else{
-        res.sendStatus(404);
-    }
-}
+  } else {
+    res.sendStatus(404);
+  }
+};
 
 module.exports = {
   renderClinicianDashboard,
--- models/db.js
+++ models/db.js
@@ -1,33 +1,29 @@
 // For database login details
-require('dotenv').config()
-const mongoose = require("mongoose")
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 console.log(process.env.PORT);
 
-const dbAddress = "mongodb+srv://Webbbbers:NKWKsAYmRdZlz27G@cluster0.p0aj3.mongodb.net/diabetes?retryWrites=true&w=majority";
-
-console.log(dbAddress);
-
-
-mongoose.connect( dbAddress, {
+mongoose
+  .connect(process.env.MONGOURL, {
     useNewUrlParser: true,
-    dbName: "diabetes"
+    dbName: "diabetes",
   })
-  .then(() => console.log('DB connection successful'));
+  .then(() => console.log("DB connection successful"));
 
-  const db = mongoose.connection
+const db = mongoose.connection;
 
-  db.on("error", err => {
-      console.error(err);
-      process.exit(1)
-  })
+db.on("error", (err) => {
+  console.error(err);
+  process.exit(1);
+});
 
-  db.once("open", async() => {
-      console.log("Mongo connection started on " + db.host + ":" + db.port)
-  })
+db.once("open", async () => {
+  console.log("Mongo connection started on " + db.host + ":" + db.port);
+});
 
-  require("./patient")
-  require("./bloodGlucose")
-  require("./weight")
-  require("./insulinTaken")
-  require("./exercise")
+require("./patient");
+require("./bloodGlucose");
+require("./weight");
+require("./insulinTaken");
+require("./exercise");
