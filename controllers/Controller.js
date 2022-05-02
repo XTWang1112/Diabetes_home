7ea1a55b9b32c62cc96860efa944c16f660cb487
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-05-02 15:16:41 +1000

    add patient

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,11 +1,12 @@
-const req = require("express/lib/request");
-const mongoose = require("mongoose");
-const patientData = require("../models/patient");
-const patientModel = mongoose.model("patients");
-const bloodGlucoseModel = mongoose.model("bloodGlucoses");
-const weightModel = mongoose.model("weights");
-const insulineTakenModel = mongoose.model("insulinTakens");
-const exerciseModel = mongoose.model("exercises");
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const mongoose = require('mongoose');
+const patientData = require('../models/patient');
+const patientModel = mongoose.model('patients');
+const bloodGlucoseModel = mongoose.model('bloodGlucoses');
+const weightModel = mongoose.model('weights');
+const insulineTakenModel = mongoose.model('insulinTakens');
+const exerciseModel = mongoose.model('exercises');
 
 // The function to redner clinician dashboard
 const renderClinicianDashboard = async (req, res) => {
@@ -26,11 +27,11 @@ const renderClinicianDashboard = async (req, res) => {
         }
       )
       .lean();
-    
+
     // Get Current date
     const today = new Date(new Date().toDateString()).getTime();
     const tomorrow = today + 24 * 3600 * 1000;
-    
+
     // Get each patient's latest blood glucose and weight  value
     for (patient of patients) {
       console.log(patient.patientName);
@@ -61,7 +62,7 @@ const renderClinicianDashboard = async (req, res) => {
           patient_result.bloodGlucose_upperBound;
       } else {
         patient.today_blood_glucose_level = 0;
-        patient.today_blood_glucose_level = "No data today";
+        patient.today_blood_glucose_level = 'No data today';
       }
 
       // sort weight value according to date and time
@@ -76,19 +77,23 @@ const renderClinicianDashboard = async (req, res) => {
         patient.weight_upper_bound = 85;
       } else {
         patient.today_weight = 0;
-        patient.today_weight = "No data today";
+        patient.today_weight = 'No data today';
       }
     }
     console.log(patients);
-    res.render("Clinician_dashboard", { patients: patients });
+    res.render('Clinician_dashboard', { patients: patients });
   } catch (err) {
     console.log(err);
   }
 };
- 
+
+const renderAddPatient = async (req, res) => {
+  res.render('Add_patient');
+};
+
 // The function to get the current value of each data and render the patient dashboard
 const renderPatientDashboard = async (req, res) => {
-  let patient_id = "6267d6bb8b206aade8b24198";
+  let patient_id = '6267d6bb8b206aade8b24198';
   // find the patient using its id
   let patient = await patientModel.findById(patient_id).lean();
   console.log(patient.patientName);
@@ -120,31 +125,30 @@ const renderPatientDashboard = async (req, res) => {
     // comments.push(bloodGlucose_result.comment)
   } else {
     patient.today_blood_glucose_level = 0;
-    patient.today_blood_glucose_level = "no data today";
+    patient.today_blood_glucose_level = 'no data today';
   }
 
   console.log(comments);
-  res.render("Patient_Dashboard", {
+  res.render('Patient_Dashboard', {
     patient,
     comments,
-    layout: "patient_template",
+    layout: 'patient_template',
   });
 };
-
 
 // The fucntion to render patient blood record page
 const renderPatientBloodRecord = async (req, res) => {
   const data = patientData;
   if (data) {
-    res.render("Blood_glucose", {
+    res.render('Blood_glucose', {
       onePatient: data,
-      layout: "patient_record_template",
+      layout: 'patient_record_template',
     });
     // request the patient comment and bloodGlucose value from the input
-    var glucose_comment = req.query.glucose_comment || "no comments";
+    var glucose_comment = req.query.glucose_comment || 'no comments';
     var patinet_blood_glucose = req.query.patinet_blood_glucose;
     if (glucose_comment && patinet_blood_glucose) {
-      let patient_id = "6267d6bb8b206aade8b24198";
+      let patient_id = '6267d6bb8b206aade8b24198';
       let patientBloodRecord = {
         patient_id,
         value: patinet_blood_glucose,
@@ -161,9 +165,9 @@ const renderPatientBloodRecord = async (req, res) => {
   }
 };
 
-
 module.exports = {
   renderClinicianDashboard,
   renderPatientDashboard,
   renderPatientBloodRecord,
+  renderAddPatient,
 };
--- routes/Clinician_Router.js
+++ routes/Clinician_Router.js
@@ -1,11 +1,12 @@
-const express = require('express')
+const express = require('express');
 
-const Router = express.Router()
+const Router = express.Router();
 
-const Controller = require('../controllers/Controller')
+const Controller = require('../controllers/Controller');
 
 // add a route to handle the get request for all patients' data
-Router.get('/', Controller.renderClinicianDashboard)
+Router.get('/', Controller.renderClinicianDashboard);
+Router.get('/add-patient', Controller.renderAddPatient);
 
 // export the router
-module.exports = Router
+module.exports = Router;
