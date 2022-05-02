4ab97a7c4d66e453640a3ff8a4c62e965a6c0f0f
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-05-02 18:56:40 +1000

    add patient page

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 // Import express-handlebars
-const exphbs = require("express-handlebars");
+const exphbs = require('express-handlebars');
 
 // Import express
-const express = require("express");
+const express = require('express');
 
 // Set app as our server
 const app = express();
 
 // configure Handlebars
 app.engine(
-  "hbs",
+  'hbs',
   exphbs.engine({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "clinician_template",
-    extname: "hbs",
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'clinician_template',
+    extname: 'hbs',
   })
 );
 
 // set Handlebars view engine
-app.set("view engine", "hbs");
+app.set('view engine', 'hbs');
 
 // connect to database
-require("./models/db.js");
+require('./models/db.js');
 
-var hbs = require("handlebars");
+var hbs = require('handlebars');
 // get system time and compare it in the handlebars view model
-hbs.registerHelper("compare", function (a, options) {
+hbs.registerHelper('compare', function (a, options) {
   var current_time = new Date();
   var current_year = current_time.getFullYear();
   var current_month = current_time.getMonth() + 1;
   var current_date = current_time.getDate();
-  var current_time = current_date + "/" + current_month + "/" + current_year;
+  var current_time = current_date + '/' + current_month + '/' + current_year;
   if (a == current_time) {
     return options.fn(this);
   } else {
@@ -38,7 +38,7 @@ hbs.registerHelper("compare", function (a, options) {
   }
 });
 
-hbs.registerHelper("compare_safety_range", function (a, b, c, options) {
+hbs.registerHelper('compare_safety_range', function (a, b, c, options) {
   // a is record value, b is lower bound, c is upper bound
   if (a >= b && a <= c) {
     return options.fn(this);
@@ -48,24 +48,26 @@ hbs.registerHelper("compare_safety_range", function (a, b, c, options) {
 });
 
 // set gloabal static resource
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
 // Set up to handle POST requests
 app.use(express.json()); // needed if POST data is in JSON format
-app.use(express.urlencoded({
-  extended: false
-})); // only needed for URL-encoded input
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+); // only needed for URL-encoded input
 
 // link to our routers
-const ClinicianRouter = require("./routes/Clinician_Router");
-const PatientRouter = require("./routes/Patient_Router");
+const ClinicianRouter = require('./routes/Clinician_Router');
+const PatientRouter = require('./routes/Patient_Router');
 
 // the demo routes are added to the end of the '/clinician' path
-app.use("/clinician", ClinicianRouter);
-app.use("/patient", PatientRouter);
+app.use('/clinician', ClinicianRouter);
+app.use('/patient', PatientRouter);
 
 // render Clinician_dashboard page
-app.get("", (req, res) => {
+app.get('', (req, res) => {
   res.send(
     "<h1>The home page is under developing, please view: <br><a href='https://webbbbers-diabetes-home.herokuapp.com/patient'>https://webbbbers-diabetes-home.herokuapp.com/patient</a><br><a href='https://webbbbers-diabetes-home.herokuapp.com/clinician'>https://webbbbers-diabetes-home.herokuapp.com/clinician</a></h1>."
   );
@@ -73,5 +75,5 @@ app.get("", (req, res) => {
 
 // Tells the app to listen on port 80 and logs that information to the console.
 app.listen(process.env.PORT || 80, () => {
-  console.log("Diabetes@Home listening on port 80");
+  console.log('Diabetes@Home listening on port 80');
 });
