84fa220160bff73d3130417cb5fb232ea1b12e2f
jiaswang <jiaswang@student.unimelb.edu.au>
2022-05-09 14:21:40 +1000

    Update ctrlerAddPatient.js

--- controllers/ctrlerAddPatient.js
+++ controllers/ctrlerAddPatient.js
@@ -1,11 +1,33 @@
 const req = require('express/lib/request');
 const res = require('express/lib/response');
+const patientModel = require('../models/patient');
 
 // const patientModel = require('../models/patient');
 
 
 const renderAddPatient = async (req, res) => {
-    res.render('Add_patient');
+    const newPatient = new patientModel({
+        firstName: req.body.firstName,
+        lastNamae: req.body.lastName,
+        Email: req.body.Email,
+        phoneNumber: req.body.phoneNumber,
+        birthday: req.body.birthday,
+        streetAddress: req.body.streetAddress,
+        postalCode: req.body.postalCode,
+        city: req.body.city,
+        photo_url: req.body.photo_url,
+
+    });
+    newPatient.save(function(err){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("addPatient")
+        }
+    });
+
+   
+    
 };
 
 
