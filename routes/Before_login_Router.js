020ccf4e07d283b2a8fc3e79659767da94759b99
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-05-12 20:38:38 +1000

    info pagese

--- routes/Before_login_Router.js
+++ routes/Before_login_Router.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const Router = express.Router();
 
-const Controller = require("../controllers/Controller");
+const Controller = require('../controllers/Controller');
 
 // add a route to handle the get request for all patients' data
-Router.get("/about-website", Controller.renderAboutWebsite);
-Router.get("/about-diabetes", Controller.renderAboutDiabetes);
+Router.get('/about-website', Controller.renderAboutWebsite);
+Router.get('/about-diabetes', Controller.renderAboutDiabetes);
+Router.get('/login', Controller.renderPatientLogin);
 
 // export the router
 module.exports = Router;
