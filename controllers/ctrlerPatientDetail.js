982b5aa7ddf5683859fc575583bda7376320ffc3
yingxi1 <62348590+Ying-xi@users.noreply.github.com>
2022-05-17 20:23:39 +0800

    fix db and patient details

--- controllers/ctrlerPatientDetail.js
+++ controllers/ctrlerPatientDetail.js
@@ -1,9 +1,15 @@
+const { handle } = require('express/lib/application');
+const req = require('express/lib/request');
+const { redirect } = require('express/lib/response');
+const res = require('express/lib/response');
+
 const patientModel = require('../models/patient');
 
 const renderPatientDetails = async (req, res) => {
   try {
+
     // 传进来的url里面带id参数即可
-    const patient = await patientModel.findById(req.params.id).lean();
+    let patient = await patientModel.findById(req.params.id).lean()
     // let patient_id = req.params.patient_id;
     // console.log("获取到的url: "+req.params.id)
 
@@ -25,30 +31,30 @@ const setTimeSeries = async (req, res) => {
     // 判定是否需要录入该数据
     // bloodGlucose的判定
     var bloodGlucose_update = req.body.bloodGlucose_record;
-    if (bloodGlucose_update == undefined) {
+    if(bloodGlucose_update == undefined){
       bloodGlucose_update = false;
-    } else {
+    }else{
       bloodGlucose_update = true;
     }
     // insulinTaken的判定
     var insulinTaken_update = req.body.insulinTaken_record;
-    if (insulinTaken_update == undefined) {
+    if(insulinTaken_update == undefined){
       insulinTaken_update = false;
-    } else {
+    }else{
       insulinTaken_update = true;
     }
     // weight的判定
     var weight_update = req.body.weight_record;
-    if (weight_update == undefined) {
+    if(weight_update == undefined){
       weight_update = false;
-    } else {
+    }else{
       weight_update = true;
     }
     // exercise的判定
-    var exercise_update = req.body.exercise_record;
-    if (exercise_update == undefined) {
+    var exercise_update = req.body.exercise_record
+    if(exercise_update == undefined){
       exercise_update = false;
-    } else {
+    }else{
       exercise_update = true;
     }
 
@@ -58,42 +64,46 @@ const setTimeSeries = async (req, res) => {
     await patientModel.updateOne(
       { _id: matchID },
       {
-        // _id: req.params.id,
-        // bloodGlucose
-        bloodGlucose_record: bloodGlucose_update,
-        bloodGlucose_lowerBound: req.body.bloodGlucose_lb,
-        bloodGlucose_upperBound: req.body.bloodGlucose_ub,
+      // _id: req.params.id,
+      // bloodGlucose
+      bloodGlucose_record: bloodGlucose_update,
+      bloodGlucose_lowerBound: req.body.bloodGlucose_lb,
+      bloodGlucose_upperBound: req.body.bloodGlucose_ub,
 
-        // insulinTaken
-        insulinTaken_record: insulinTaken_update,
-        insulinTaken_lowerBound: req.body.insulinTaken_lb,
-        insulinTaken_upperBound: req.body.insulinTaken_ub,
+      // insulinTaken
+      insulinTaken_record: insulinTaken_update,
+      insulinTaken_lowerBound: req.body.insulinTaken_lb,
+      insulinTaken_upperBound: req.body.insulinTaken_ub,
 
-        // weight
-        weight_record: weight_update,
-        weight_lowerBound: req.body.weight_lb,
-        weight_upperBound: req.body.weight_ub,
+      // weight
+      weight_record: weight_update,
+      weight_lowerBound: req.body.weight_lb,
+      weight_upperBound: req.body.weight_ub,
 
-        // exercise
-        exercise_record: exercise_update,
-        exercise_lowerBound: req.body.exercise_lb,
-        exercise_upperBound: req.body.exercise_ub,
-      }
-    );
-    console.log('更新完毕');
+      // exercise
+      exercise_record: exercise_update,
+      exercise_lowerBound: req.body.exercise_lb,
+      exercise_upperBound: req.body.exercise_ub,
+    });
+    console.log("更新完毕");
 
     // 重新定向避免无限循环
     res.redirect('/clinician/' + req.params.id);
+
   } catch (err) {
-    console.log('出错了：' + err);
+    console.log("出错了：" + err);
     res.status(404).json({
       status: 'fail',
       message: err,
     });
   }
 };
+
+
+
+
 const saveSupportMessage = async (req, res) => {
-  console.log('saveSupportMessage running');
+  console.log("saveSupportMessage running")
   // let patient_id = '6267d6bb8b206aade8b24198';
   let patient_id = req.params.id;
   let message_date = new Date();
@@ -102,11 +112,8 @@ const saveSupportMessage = async (req, res) => {
     .updateOne({ _id: patient_id }, { support_message: message })
     .then((result) => console.log('Try to change support message'));
   await patientModel
-    .updateOne(
-      { _id: patient_id },
-      { support_message_date: message_date.toLocaleDateString() }
-    )
-    .then((result) => console.log('Try to change support message date'));
+  .updateOne({ _id: patient_id }, { support_message_date: message_date.toLocaleDateString() })
+  .then((result) => console.log('Try to change support message date'));
 
   // 重新定向
   res.redirect('/clinician/' + req.params.id);
