f27d338bd334cd137dfe99cfaf566026bb6836c8
yingxi1 <62348590+Ying-xi@users.noreply.github.com>
2022-05-17 18:58:19 +0800

    update patient detail

--- controllers/ctrlerPatientDetail.js
+++ controllers/ctrlerPatientDetail.js
@@ -1,5 +1,6 @@
 const { handle } = require('express/lib/application');
 const req = require('express/lib/request');
+const { redirect } = require('express/lib/response');
 const res = require('express/lib/response');
 
 const patientModel = require('../models/patient');
@@ -25,62 +26,39 @@ const renderPatientDetails = async (req, res) => {
 
 const setTimeSeries = async (req, res) => {
   try {
-
     let patient = await patientModel.findById(req.params.id).lean();
-    // console.log("体重的lb是：" + req.body.weight_lb)
-    // console.log("全部数据是：" + req.body)
 
-    // patientModel.updateOne({ weight_lowerBound: req.body.weight_lb })
-
-    // console.log("病人： " + patient);
-    // var test_id = "628366b430c62f1b691951e0";
-    // console.log("病人id： " + test_id);
-
-    var bloodGlucose_update = req.body.bloodGlucose_record;
+    // 判定是否需要录入该数据
     // bloodGlucose的判定
+    var bloodGlucose_update = req.body.bloodGlucose_record;
     if(bloodGlucose_update == undefined){
       bloodGlucose_update = false;
     }else{
       bloodGlucose_update = true;
     }
-
-    var insulinTaken_update = req.body.insulinTaken_record;
     // insulinTaken的判定
+    var insulinTaken_update = req.body.insulinTaken_record;
     if(insulinTaken_update == undefined){
       insulinTaken_update = false;
     }else{
       insulinTaken_update = true;
     }
-
-    var weight_update = req.body.weight_record;
     // weight的判定
+    var weight_update = req.body.weight_record;
     if(weight_update == undefined){
       weight_update = false;
     }else{
       weight_update = true;
     }
-
-    var exercise_update = req.body.exercise_record
     // exercise的判定
+    var exercise_update = req.body.exercise_record
     if(exercise_update == undefined){
       exercise_update = false;
     }else{
       exercise_update = true;
     }
 
-    console.log("personalid :" + req.params.id);
-
-    console.log("血糖测试" + bloodGlucose_update);
-    console.log("血糖lb测试" + req.body.bloodGlucose_lb);
-    console.log("血糖ub测试" + req.body.bloodGlucose_ub);
-
-    var patient_id = "628366b430c62f1b691951e0";
-    // patientModel
-    // .updateOne({ _id: patient_id }, { bloodGlucose_record: bloodGlucose_update }).save();
-
-
-    console.log("第一次update完毕");
-
+    // 更新数据
     const matchID = req.params.id;
 
     await patientModel.updateOne(
@@ -108,13 +86,10 @@ const setTimeSeries = async (req, res) => {
       exercise_upperBound: req.body.exercise_ub,
     });
     console.log("更新完毕");
-    // 以下为测试输出，可删
-    // const weight_lb = req.body.weight_lb;
-    // const weight_ub = req.body.weight_ub;
-    // console.log("测试判定: " + weight_record);
-    // console.log("测试布尔值: " + req.body.weight_record);
-    // console.log("测试lb" + weight_lb);
-    // console.log("测试ub" + weight_ub);
+
+    // 重新定向避免无限循环
+    res.redirect('/clinician/' + req.params.id);
+
   } catch (err) {
     console.log("出错了：" + err);
     res.status(404).json({
@@ -129,7 +104,8 @@ const setTimeSeries = async (req, res) => {
 
 const saveSupportMessage = async (req, res) => {
   console.log("saveSupportMessage running")
-  let patient_id = '6267d6bb8b206aade8b24198';
+  // let patient_id = '6267d6bb8b206aade8b24198';
+  let patient_id = req.params.id;
   let message_date = new Date();
   let message = req.body.support_message;
   await patientModel
@@ -138,10 +114,13 @@ const saveSupportMessage = async (req, res) => {
   await patientModel
   .updateOne({ _id: patient_id }, { support_message_date: message_date.toLocaleDateString() })
   .then((result) => console.log('Try to change support message date'));
+
+  // 重新定向
+  res.redirect('/clinician/' + req.params.id);
 };
 
 module.exports = {
   renderPatientDetails,
   setTimeSeries,
-  // saveSupportMessage,
+  saveSupportMessage,
 };
