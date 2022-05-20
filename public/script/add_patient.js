86c239241de7361936ab000654a1e412ef1b0b8d
XTWang1112 <54648260+XTWang1112@users.noreply.github.com>
2022-05-20 15:49:33 +1000

    add patient frontend bug

--- public/script/add_patient.js
+++ public/script/add_patient.js
@@ -11,6 +11,7 @@ document.querySelector('.overlay').addEventListener('click', closeWindow);
 /////////////////////////
 //pop message window
 if (document.querySelector('.password').textContent.length > 0) {
+  console.log(document.querySelector('.password').textContent);
   document.querySelector('.account-password').classList.remove('disappear');
   document.querySelector('.overlay').classList.remove('disappear');
 }
