59444a20b50b49711fca7fc336505ddafe15a426
Zihan YU <deepmemorysea@163.com>
2022-05-20 18:36:35 +0800

    fix back password message

--- routes/Before_login_Router.js
+++ routes/Before_login_Router.js
@@ -15,33 +15,29 @@ Router.get('/about-website', Controller.renderAboutWebsite);
 Router.get('/about-diabetes', Controller.renderAboutDiabetes);
 Router.get('/clinician-login', Controller.renderClinicianLogin);
 Router.post(
-    '/clinician-login',
-    utility.unLoggedIn,
-    passport.authenticate("clinician-login", {
-        failureRedirect: "/guest/clinician-login",
-        failureFlash: true,
-    }),
-    (req, res) => {
-        console.log("这里是医生")
-        res.redirect('/clinician')
-    }
+  '/clinician-login',
+  utility.unLoggedIn,
+  passport.authenticate("clinician-login", {
+      failureRedirect: "/guest/clinician-login",
+      failureFlash: true,
+  }),
+  (req, res) => {
+      console.log("这里是医生")
+      res.redirect('/clinician')
+  }
 
 )
 Router.get('/login', utility.unLoggedIn, Controller.renderPatientLogin);
-Router.get(
-  '/login/tryagine',
-  utility.unLoggedIn,
-  Controller.renderPatientLoginTryagin
-);
+Router.get('/login/tryagine', utility.unLoggedIn, Controller.renderPatientLoginTryagin);
 // Process login attempt
 Router.post(
   '/login',
   utility.unLoggedIn,
   passport.authenticate('patient-login', {
     /* successRedirect: '/patient/62864c7a96d8e97f2078fc9c', */
-    failureRedirect: '/guest/login',
-    failureFlash: true,
-  }), // 如果是坏人，重新回到login page
+    failureRedirect: '/guest/login/tryagine',
+    failureflash: true,
+  }), 
   (req, res) => {
     console.log('用户 ');
     res.redirect('/patient/' + req.user._id.toString());
